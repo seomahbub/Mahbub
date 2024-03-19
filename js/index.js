e32210e5001d6395f2d929df1fcb7dd00cb9a92a
Md Mahbubur Rahman <164078720+seomahbub@users.noreply.github.com>
2024-03-20 04:03:25 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// sticky navbar
+
+var navbar=document.getElementById('navbar');
+
+window.addEventListener('scroll',()=>
+{
+    if(window.scrollY > 50){
+        navbar.classList.add('sticky')
+    }
+else{
+    navbar.classList.remove('sticky')
+}
+}
+)
